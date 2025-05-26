cdb8da3acd23f4cea3014413c526c4b9d92ef644
nhungtong <tongthihongnhung9999@gmail.com>
2025-05-26 11:19:43 +0700

    add export excel by date

--- src/main/java/com/techbytedev/warehousemanagement/controller/StockCheckController.java
+++ src/main/java/com/techbytedev/warehousemanagement/controller/StockCheckController.java
@@ -2,13 +2,18 @@ package com.techbytedev.warehousemanagement.controller;
 
 import com.techbytedev.warehousemanagement.dto.request.StockCheckRequest;
 import com.techbytedev.warehousemanagement.dto.response.StockCheckResponse;
-import com.techbytedev.warehousemanagement.entity.User;
 import com.techbytedev.warehousemanagement.service.StockCheckService;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -44,5 +49,29 @@ public class StockCheckController {
     public ResponseEntity<List<StockCheckResponse>> getMonthStockChecks() {
         List<StockCheckResponse> result = stockCheckService.getMonthStockChecks();
         return ResponseEntity.ok(result);
+    }
+    // Lấy danh sách kiểm kê theo ngày
+    @GetMapping("/by-date")
+    @PreAuthorize("@permissionChecker.hasPermission(authentication, '/api/stockcheck/**', 'GET')")
+    public ResponseEntity<List<StockCheckResponse>> getStockChecksByDate(
+            @RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+        List<StockCheckResponse> responses = stockCheckService.getStockCheckResponsesByDate(date);
+        return ResponseEntity.ok(responses);
+    }
+    // xuất excel theo ngày
+    @GetMapping("/export-excel")
+    @PreAuthorize("@permissionChecker.hasPermission(authentication, '/api/stockcheck/**', 'GET')")
+    public ResponseEntity<InputStreamResource> exportStockCheckExcel(
+            @RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) throws IOException {
+
+        ByteArrayInputStream in = stockCheckService.exportStockCheckExcelByDate(date);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "attachment; filename=stock_check_" + date + ".xlsx");
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentType(MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"))
+                .body(new InputStreamResource(in));
     }
 }
